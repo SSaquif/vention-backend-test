a17c0e1b14063d7e1f4015ea5a718653764f6cbf
Sadnan Saquif <SSaquif@users.noreply.github.com>
2022-01-21 17:12:40 -0500

    added dependencies morgan and nodemon

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 const server = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
-const materialsRouter = require('./routes/materialsRouter.js');
+const materialsRouter = require("./routes/materialsRouter.js");
 
 // Middleware
 server.use(cors());
 server.use(express.json());
+server.use(morgan("dev"));
 
 // Routers
-server.use('/api/material', materialsRouter);
+server.use("/api/material", materialsRouter);
 
 //Routes
-server.get('/', (req, res) => {
-  res.status(200).send('Vention Quest');
+server.get("/", (req, res) => {
+  res.status(200).send("Vention Quest");
 });
 
 module.exports = server;
